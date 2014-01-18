e9c8899073e8397992c13385ca852397e1168d30
Janne Haapsaari <haaja@iki.fi>
2014-01-18 15:32:05 +0200

    Implement average_ratings method
    The number of ratings and average ratings is listed on beers show page.
    
    Signed-off-by: Janne Haapsaari <haaja@iki.fi>

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,13 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    avg = 0.0
+    ratings.each do |rating|
+      avg += rating.score
+    end
+    return avg/ratings.count
+  end
 end
+
