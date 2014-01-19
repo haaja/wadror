e54b56eee50721f2094ced333d096f7e11accbc2
Janne Haapsaari <haaja@iki.fi>
2014-01-19 23:28:03 +0200

    Implement average_rating on brewery
    The average rating is listed on brewery page if the brewery has ratings.

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,7 +1,14 @@
 class Brewery < ActiveRecord::Base
-  has_many :beers, :dependent => :destroy
+  has_many :beers
+  has_many :ratings, :through => :beers
 
   def to_s
     return "#{name}"
+  end
+
+  def average_rating
+    # this is simpler but the assignment required using inject
+    #ratings.average(:score)
+    ratings.inject(0.0) { |sum, rating | sum + rating.score } / ratings.count
   end
 end
