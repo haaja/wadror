9bfe19151ac98d8359a7e2ccce21f818d75a9c3c
Janne Haapsaari <haaja@iki.fi>
2014-01-26 23:38:18 +0200

    int -> integer

--- db/migrate/20140126213005_create_beer_clubs.rb
+++ db/migrate/20140126213005_create_beer_clubs.rb
@@ -2,7 +2,7 @@ class CreateBeerClubs < ActiveRecord::Migration
   def change
     create_table :beer_clubs do |t|
       t.string :username
-      t.int :founded
+      t.integer :founded
       t.string :city
 
       t.timestamps
