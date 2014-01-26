8270c916cdf19cea9444f6300c31af4aa9908b85
Janne Haapsaari <haaja@iki.fi>
2014-01-26 20:42:30 +0200

    Validate that brewery name is present when creating a brewery
    Also the founding year must be between 1042 and 2014.

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,6 +1,11 @@
 class Brewery < ActiveRecord::Base
   include RatingAverage
 
+  validates_presence_of :name
+  validates :year, presence: true,
+                   numericality: { greater_than_or_equal_to: 1042,
+                                   less_than_or_equal_to: 2014,
+                                   only_integer: true }
   has_many :beers
   has_many :ratings, :through => :beers
 
