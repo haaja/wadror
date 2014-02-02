6d540766e752ebf7ecf91c7cde3637b2531de0fa
Janne Haapsaari <haaja@iki.fi>
2014-02-02 18:14:38 +0200

    More double quotes to single quotes

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe Beer do
-  it "has the name and style set" do
-    beer = Beer.create name:"Test beer", style:"Lager"
+  it 'has the name and style set' do
+    beer = Beer.create name:'Test beer', style:'Lager'
 
-    beer.name.should == "Test beer"
-    beer.style.should == "Lager"
+    beer.name.should == 'Test beer'
+    beer.style.should == 'Lager'
     expect(beer).to be_valid
     expect(Beer.count).to eq(1)
   end
 
-  it "is not saved without name" do
-    beer = Beer.create style:"Lager"
+  it 'is not saved without name' do
+    beer = Beer.create style:'Lager'
 
     expect(beer).not_to be_valid
     expect(Beer.count).to eq(0)
