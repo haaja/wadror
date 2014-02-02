d6b657bfb825365334bd82bcc6a19a80f9c7c0c4
Janne Haapsaari <haaja@iki.fi>
2014-02-03 01:33:59 +0200

    Test that user is redirected back to login page when incorrect credentials are given

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -15,5 +15,15 @@ describe 'User' do
       expect(page).to have_content 'Welcome back!'
       expect(page).to have_content 'Pekka'
     end
+
+    it 'is redirected back to signin from if wrong credentials are given' do
+      visit signin_path
+      fill_in('username', with: 'Pekka')
+      fill_in('password', with: 'WillNotWork')
+      click_button('Log in')
+
+      expect(current_path).to eq(signin_path)
+      expect(page).to have_content 'username and password do not match'
+    end
   end
 end
