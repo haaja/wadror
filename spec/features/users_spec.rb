dcb9e40c6781a5e6484adadd0e8a2f18e67766a2
Janne Haapsaari <haaja@iki.fi>
2014-02-03 01:38:35 +0200

    Test that successful signup will increase the user count

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -25,5 +25,16 @@ describe 'User' do
       expect(current_path).to eq(signin_path)
       expect(page).to have_content 'username and password do not match'
     end
+
+    it 'when signed up with good credentials, is added to the system' do
+      visit signup_path
+      fill_in('user_username', with: 'Brian')
+      fill_in('user_password', with: 'Secret55')
+      fill_in('user_password_confirmation', with: 'Secret55')
+
+      expect {
+        click_button('Create User')
+      }.to change{ User.count }.by(1)
+    end
   end
 end
