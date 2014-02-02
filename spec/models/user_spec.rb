ef066b70ef15b6b37a1aca48511c2c2fd64c59e0
Janne Haapsaari <haaja@iki.fi>
2014-02-02 17:30:38 +0200

    Test that constructor sets the username correctly

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has the username set correctly" do
+    user = User.new username:"Pekka"
+
+    user.username.should == "Pekka"
+  end
 end
