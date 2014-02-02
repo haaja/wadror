52e5000658d55d4090ab472abd59eaed0f3d7ff8
Janne Haapsaari <haaja@iki.fi>
2014-02-02 17:32:14 +0200

    Test that User is not saved without a password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,11 @@ describe User do
 
     user.username.should == "Pekka"
   end
+
+  it "is not saved without a password" do
+    user = User.create username:"Pekka"
+
+    expect(user.valid?).to be(false)
+    expect(User.count).to eq(0)
+  end
 end
