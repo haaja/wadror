c1a21824021ce052d7364ab1f7ba474980622994
Janne Haapsaari <haaja@iki.fi>
2014-02-02 17:56:27 +0200

    Test that name and style is set correctly

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -4,6 +4,8 @@ describe Beer do
   it "has the name and style set" do
     beer = Beer.create name:"Test beer", style:"Lager"
 
+    beer.name.should == "Test beer"
+    beer.style.should == "Lager"
     expect(beer).to be_valid
     expect(Beer.count).to eq(1)
   end
