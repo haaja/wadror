844653fc23211d83990c44aacbade6b5e23c765f
Janne Haapsaari <haaja@iki.fi>
2014-02-02 18:49:24 +0200

    Implement favorite_beer for user
    Also wrote unit tests for favorite_beer.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,4 +44,35 @@ describe User do
       expect(user.average_rating).to eq(15.0)
     end
   end
+
+  describe 'favorite beer' do
+    let(:user){ FactoryGirl.create(:user) }
+
+    it 'has method for determining the favorite_beer' do
+      user.should respond_to :favorite_beer
+    end
+
+    it 'without rating does not have a favourite beer' do
+      expect(user.favorite_beer).to eq(nil)
+    end
+
+    it 'is the only rated if only one rating' do
+      beer = FactoryGirl.create(:beer)
+      rating = FactoryGirl.create(:rating, beer:beer, user:user)
+
+      expect(user.favorite_beer).to eq(beer)
+    end
+
+    it 'is the one with highest rating if several rated' do
+      beer1 = FactoryGirl.create(:beer)
+      beer2 = FactoryGirl.create(:beer)
+      beer3 = FactoryGirl.create(:beer)
+
+      rating1 = FactoryGirl.create(:rating, beer:beer1, user:user)
+      rating2 = FactoryGirl.create(:rating, score:25, beer:beer2, user:user)
+      rating3 = FactoryGirl.create(:rating, score:9, beer:beer3, user:user)
+
+      expect(user.favorite_beer).to eq(beer2)
+    end
+  end
 end
