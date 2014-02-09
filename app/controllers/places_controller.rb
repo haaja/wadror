9a673fe5ea3c815df6846e4b49a6809d7f11e743
Janne Haapsaari <haaja@iki.fi>
2014-02-09 21:05:45 +0200

    URL encode search parameter

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,7 +5,7 @@ class PlacesController < ApplicationController
   def search
     api_key = '1c062c8dced5c4374a302f0810a1f46a'
     url = "http://beermapping.com/webservice/loccity/#{api_key}/"
-    response = HTTParty.get "#{url}#{params[:city]}"
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(params[:city])}"
     places = response.parsed_response['bmp_locations']['location']
 
     if places.is_a?(Hash) and places['id'].nil?
