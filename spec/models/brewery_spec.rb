2a211206f0a7e34a4ebad10d429f81c7d1fef06d
Janne Haapsaari <haaja@iki.fi>
2014-02-09 15:26:00 +0200

    Add same test but using newer syntax

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -18,4 +18,13 @@ describe Brewery do
 
     expect(brewery).not_to be_valid
   end
+
+  # same test but using newer syntax
+  describe 'when initialized with name Schlenkerla and year 1674' do
+    subject{ Brewery.create name: 'Schlenkerla', year: 1674 }
+
+    it { should be_valid }
+    its(:name) { should eq('Schlenkerla') }
+    its(:year) { should eq(1674) }
+  end
 end
