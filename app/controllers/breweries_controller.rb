60a9c93755748c95669356f86e47cfa0ffa9546b
Janne Haapsaari <haaja@iki.fi>
2014-02-09 16:56:13 +0200

    Ensure that user is signed in for certain operations

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,5 +1,5 @@
 class BreweriesController < ApplicationController
-  before_filter :authenticate, only: [:destroy]
+  before_action :ensure_that_signed_in, except: [:index, :show]
   before_action :set_brewery, only: [:show, :edit, :update]
 
 
@@ -73,13 +73,5 @@ class BreweriesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def brewery_params
       params.require(:brewery).permit(:name, :year)
-    end
-
-    def authenticate
-      admin_accounts = { 'admin' => 'secret', 'pekka' => 'beer', 'arto' => 'foobar', 'matti' => 'ittam' }
-
-      authenticate_or_request_with_http_basic do |username, password|
-        admin_accounts[username] and admin_accounts[username] == password
-      end
     end
 end
