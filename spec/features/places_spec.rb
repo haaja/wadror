08444eb81aad38957a13185754b413c67ab43269
Janne Haapsaari <haaja@iki.fi>
2014-02-09 21:25:14 +0200

    If API returns multiple results, they should all be shown on the page

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -12,4 +12,22 @@ describe 'Places' do
 
     expect(page).to have_content 'Oljenkorsi'
   end
+
+  it 'if multiple results is returned by the API, they are all shown on the page' do
+    BeermappingApi.stub(:places_in).with('kumpula').and_return(
+        [
+            Place.new(:name => 'Oljenkorsi'),
+            Place.new(:name => 'Laskuvarjo'),
+            Place.new(:name => 'Onnenpekka')
+        ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button 'Search'
+
+    expect(page).to have_content 'Oljenkorsi'
+    expect(page).to have_content 'Laskuvarjo'
+    expect(page).to have_content 'Onnenpekka'
+  end
 end
