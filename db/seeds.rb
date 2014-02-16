dcf71223cc8d189bfa50fffcbea6430e4ff2fb5c
Janne Haapsaari <haaja@iki.fi>
2014-02-17 00:05:23 +0200

    Create styles in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,18 @@ b1 = Brewery.create name:"Koff", year:1897
 b2 = Brewery.create name:"Malmgard", year:2001
 b3 = Brewery.create name:"Weihenstephaner", year:1042
 
-b1.beers.create name:"Iso 3", style:"Lager"
-b1.beers.create name:"Karhu", style:"Lager"
-b1.beers.create name:"Tuplahumala", style:"Lager"
-b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
-b2.beers.create name:"X Porter", style:"Porter"
-beer3 = b3.beers.create name:"Hefezeizen", style:"Weizen"
-beer4 = b3.beers.create name:"Helles", style:"Lager"
+s1 = Style.create name:"Lager", description:"Lager is a type of beer that is fermented and conditioned at low temperatures."
+s2 = Style.create name:"Pale ale", description:"Pale ale is a beer made by warm fermentation using predominantly pale malt."
+s3 = Style.create name:"Weizen", description:"Wheat beer is a beer that is brewed with a large proportion of wheat in addition to malted barley."
+s4 = Style.create name:"Porter", description:"Porter is a dark style of beer originating in London in the 18th century, descended from brown beer, a well hopped beer made from brown malt."
+
+b1.beers.create name:"Iso 3", style: s1
+b1.beers.create name:"Karhu", style: s1
+b1.beers.create name:"Tuplahumala", style: s1
+b2.beers.create name:"Huvila Pale Ale", style: s2
+b2.beers.create name:"X Porter", style: s4
+beer3 = b3.beers.create name:"Hefezeizen", style: s3
+beer4 = b3.beers.create name:"Helles", style: s1
 
 u1 = User.create username:"Test", password:"SalaK4l4", password_confirmation:"SalaK4l4"
 u2 = User.create username:"TestTest", password:"SalaK4l4SalaK4l4", password_confirmation:"SalaK4l4SalaK4l4"
